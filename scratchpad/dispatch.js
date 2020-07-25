a043b2f92a61447a86d09eb71413e88fb06fa2bf
Deepak Vishwakarma <29451111+deebeast@users.noreply.github.com>
2020-07-26 02:15:39 +0530

    generice os detect launch

--- scratchpad/dispatch.js
+++ scratchpad/dispatch.js
@@ -1,3 +1,9 @@
+var platform = process.platform;
 var childProcess = require("child_process");
-childProcess.execSync("start cmd.exe /K node post_jwt.js");
-process.exit(0)
+var configurations = require("../configurations/configurations.json");
+if (platform == "win32") {
+    childProcess.execSync(`start ${configurations.terminal_name} /K run_n.bat`);
+} else {
+    console.log(process.platform);
+}
+process.exit(0);
