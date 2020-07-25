4fd172afce0da9898793c754085332a2eb002f44
Deepak Vishwakarma <29451111+deebeast@users.noreply.github.com>
2020-07-25 23:43:55 +0530

    First Commit
    It is the first working commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var api_call_back = require('./api_call_back').api_call_back;
+var configurations = require("./configurations/configurations.json")
+
+/**
+ * set interval == token expiry time
+ * 
+ * concurrent request interval
+ */
+
+setInterval(api_call_back, configurations.concurrent_request_interval);
