de9eb3c6777a8034d7b567ed263f9964393798ad
Deepak Vishwakarma <29451111+deebeast@users.noreply.github.com>
2020-07-26 01:31:42 +0530

    added os configurations

--- run_n.bat
+++ run_n.bat
@@ -3,9 +3,10 @@ node configurations.js
 set /p num_of_users=<configurations/num_of_users
 set /p progressive_load=<configurations/progressive_load
 set /p progressive_load_timeout=<configurations/progressive_load_timeout
+set /p terminal_name=<configurations/terminal_name
 REM number of users loop
 FOR /L %%A IN (1,1,%num_of_users%) DO (
-    start cmd.exe /K node index.js
+    start %terminal_name% /K node index.js
     REM maintain wait between concurrent user requests
     IF %progressive_load%==true (
         timeout /t %progressive_load_timeout% /nobreak
--- run_n.sh
+++ run_n.sh
@@ -5,14 +5,15 @@
 num_of_users=$(<configurations/num_of_users)
 progressive_load=$(<configurations/progressive_load)
 progressive_load_timeout=$(<configurations/progressive_load_timeout)
+terminal_name=$(<configurations/terminal_name)
+shell_name=$(<configurations/shell_name)
 echo $num_of_users $progressive_load $progressive_load_timeout
 # number of users loop
 for (( c=0; c<$num_of_users; c++ ))
 do
-  gnome-terminal -e 'node index.js'
+  $terminal_name -e "$shell_name -c \"node index.js; exec $shell_name\""
   # maintain wait between concurrent user requests
   if [ "$progressive_load" == "true" ]; then
-    echo $progressive_load
+    sleep $progressive_load_timeout
   fi
-  sleep $progressive_load_timeout
 done
